f90c73c7c2c516235eaeeed31b7bbceb3ca79060
fortunatozm <fortunatompongo@hotmail.com>
2023-02-07 21:31:26 -0300

    03 - Escreva testes para cobrir 30% da camada de Service - OK - 2

---
+++ tests/unit/Services/ServiceCar.test.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import ICar from '../../../src/Interfaces/ICar';
+import Car from '../../../src/Domains/Car';
+import ServiceCar from '../../../src/Services/ServiceCar';
+
+describe('Testes que cubram 30% da camada service', function () {
+  it('Retorna todos dados com sucesso', async function () {
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    
+    const outPut: Car = new Car({
+      id: '6348513f34c397abcad040b2',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    });
+
+    sinon.stub(Model, 'create').resolves(outPut);
+    sinon.stub(Model, 'findOne').resolves(outPut);
+
+    const service = new ServiceCar();
+    const result = await service.inserCar(carInput);
+
+    expect(result).to.be.deep.equal(outPut);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
--- tests/unit/Services/ServiceMoto.test.ts
+++ tests/unit/Services/ServiceMoto.test.ts
@@ -1,38 +1,43 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
-import ICar from '../../../src/Interfaces/ICar';
-import Car from '../../../src/Domains/Car';
-import ServiceCar from '../../../src/Services/ServiceCar';
+import IMoto from '../../../src/Interfaces/IMotorcycle';
+import Moto from '../../../src/Domains/Motorcycle';
+import ServiceMoto from '../../../src/Services/ServiceMoto';
 
-describe('Testes que cubram 30% da camada service', function () {
-  it('Retorna todos dados com sucesso', async function name() {
-    const carInput: ICar = {
-      model: 'Marea',
-      year: 2002,
-      color: 'Black',
+describe('Testes que cubram 30% da camada service [Motorcycle]', function () {
+  it('Cria todos dados com sucesso [Motorcycle]', async function () {
+    const motoInput: IMoto = {
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
       status: true,
-      buyValue: 15.990,
-      doorsQty: 4,
-      seatsQty: 5,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
     };
     
-    const outPut: Car = new Car({
+    const motoOutPut: Moto = new Moto({
       id: '6348513f34c397abcad040b2',
-      model: 'Marea',
-      year: 2002,
-      color: 'Black',
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
       status: true,
-      buyValue: 15.990,
-      doorsQty: 4,
-      seatsQty: 5,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
     });
 
-    sinon.stub(Model, 'create').resolves(outPut);
+    sinon.stub(Model, 'create').resolves(motoOutPut);
+    sinon.stub(Model, 'findOne').resolves(motoOutPut);
 
-    const service = new ServiceCar();
-    const result = await service.inserCar(carInput);
+    const service = new ServiceMoto();
+    const result = await service.inserMoto(motoInput);
 
-    expect(result).to.be.deep.equal(outPut);
+    expect(result).to.be.deep.equal(motoOutPut);
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
